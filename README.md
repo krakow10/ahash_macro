548dc552dc6c970dee1455f08b5c84c969619b96
Quaternions <krakow20@gmail.com>
2025-04-25 14:12:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ahash_macro
+===========
+
+A macro which invokes ahash on a string literal.
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use proc_macro::{TokenTree,TokenStream};
+use syn::parse_macro_input;
+
+fn make_hash(value:&str)->u64{
+	use std::hash::Hasher;
+	let mut hasher=ahash::AHasher::default();
+	hasher.write(value.as_bytes());
+	hasher.finish()
+}
+
+/// Takes a string literal as input and hashes it, returning a u64 literal.
+#[proc_macro]
+pub fn hash_literal(item:TokenStream)->TokenStream{
+	let string_literal=parse_macro_input!(item as syn::LitStr);
+	let string=string_literal.value();
+	let hash=make_hash(&string);
+	let u64_literal=proc_macro::Literal::u64_suffixed(hash);
+	TokenStream::from(TokenTree::from(u64_literal))
+}
